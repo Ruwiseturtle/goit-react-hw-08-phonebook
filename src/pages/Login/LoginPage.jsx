bbaab3a430d727b9d15f70efb104878cec253e06
Matviienko Ruslana <brauniesrus@gmail.com>
2023-11-04 23:35:16 +0200

    sdfsdfsd

--- src/pages/Login/LoginPage.jsx
+++ src/pages/Login/LoginPage.jsx
@@ -3,26 +3,22 @@ import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { selectAuthenticated } from '../../redux/authSelectors';
 import './LoginPage.css';
-import LoginForm from "../../components/LoginForm/LoginForm";
+import LoginForm from '../../components/LoginForm/LoginForm';
 
 const Authentication = () => {
   const navigate = useNavigate();
   const authenticated = useSelector(selectAuthenticated);
-  
+
   useEffect(() => {
     if (authenticated) {
       navigate('/contacts');
     }
   }, [authenticated, navigate]);
-  
-  
 
   return (
-    <div className='loginPageBody'>
-      <LoginForm/>
+    <div className="loginPageBody">
+      <LoginForm />
     </div>
-      
-    
   );
 };
 
--- src/pages/Register/RegisterPage.jsx
+++ src/pages/Register/RegisterPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import  RegisterForm  from "../../components/RegisterForm/RegisterForm";
+import './RegisterPage.css';
 
 const RegisterPage = () => { 
 
