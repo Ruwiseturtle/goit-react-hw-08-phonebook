36dde3ae9103a067a90ca23bc7e312de40ff8337
Matviienko Ruslana <brauniesrus@gmail.com>
2023-11-04 23:42:43 +0200

    vzvfdv

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,18 +3,15 @@ import  './HomePage.css';
 
 const HomePage = () => {
   return (
-    <div className='containerHomePage'>
+    <div className="containerHomePage">
       <div>
+        <p className='textTitle'>Welcome to the CONTACT BOOK website!</p>
         <p className="textHome">
-                  Welcome to the CONTACT BOOK website!
-                  Here is you can save the phone
-          numbers of your friends and acquaintances. To start using the site,
-          log in. If you are not authorized, you must first register and then
-          log in. After this, you can save the contacts of your friends and not
-          worry if they get lost on your phone or notepad. Your contact data is
-                  always safe with us!
-                  
-                  
+          Here is you can save the phone numbers of your friends and
+          acquaintances. To start using the site, log in. If you are not
+          authorized, you must first register and then log in. After this, you
+          can save the contacts of your friends and not worry if they get lost
+          on your phone or notepad. Your contact data is always safe with us!
         </p>
       </div>
     </div>
